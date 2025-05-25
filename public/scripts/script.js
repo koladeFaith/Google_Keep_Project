d66bb5133daf1e4dcf443986e4b392a232907130
Kolade Faith <koladefaith1000@gmail.com>
2025-05-25 07:21:18 +0100

    Work done

---
+++ public/scripts/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAfh8yKY_gXwzAbQ4HDt4uyKLAkNBkzhXI",
+    authDomain: "keep-e986b.firebaseapp.com",
+    databaseURL: "https://keep-e986b-default-rtdb.firebaseio.com",
+    projectId: "keep-e986b",
+    storageBucket: "keep-e986b.firebasestorage.app",
+    messagingSenderId: "746391927158",
+    appId: "1:746391927158:web:739393ce1bdbc3b945a257"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
